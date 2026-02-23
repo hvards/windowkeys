hvards/windowkeys

commit 3e45ba6e91353169a8ab31972624f1f3827f45f7

Reuse Windows for Faster Activation (#2)
Avoid creating new Windows on every activation. Enabling hidden Windows
is substantially faster.

WindowKeys/Forms/OverlayForm.cs
@@ -8,22 +8,35 @@ public class OverlayForm : Form
 {
 	private readonly OverlaySettings _settings;
 	private readonly IGeometry _geometry;
-	private string _activationString;
-	private readonly RECT _rect;
-	private readonly IReadOnlyList<RECT> _occludingRects;
+	private string _activationString = string.Empty;
+	private RECT _rect;
+	private IReadOnlyList<RECT> _occludingRects = [];
 
 	private const int BORDER_MARGIN = 16;
 
-	public OverlayForm(RECT rect, string activationString, OverlaySettings settings, IGeometry geometry,
-		IReadOnlyList<RECT> occludingRects)
+	public OverlayForm(OverlaySettings settings, IGeometry geometry)
 	{
 		_settings = settings;
 		_geometry = geometry;
-		_activationString = activationString;
+
+		FormBorderStyle = FormBorderStyle.None;
+		StartPosition = FormStartPosition.Manual;
+		ShowInTaskbar = false;
+		BackColor = Color.Black;
+		Opacity = _settings.Opacity;
+	}
+
+	public void Configure(RECT rect, string activationString, IReadOnlyList<RECT> occludingRects)
+	{
 		_rect = rect;
+		_activationString = activationString;
 		_occludingRects = occludingRects;
 
-		SetFormAppearanceAndPosition();
+		Size = new Size(
+			_rect.Right - _rect.Left - BORDER_MARGIN,
+			_rect.Bottom - _rect.Top - BORDER_MARGIN
+		);
+		Location = new Point(_rect.Left + BORDER_MARGIN / 2, _rect.Top + BORDER_MARGIN / 2);
 	}
 
 	protected override CreateParams CreateParams
@@ -34,22 +47,6 @@ public class OverlayForm : Form
 			cp.ExStyle |= 0x08000000; // WS_EX_NOACTIVATE 
 			return cp;
 		}
-	}
-
-	private void SetFormAppearanceAndPosition()
-	{
-		FormBorderStyle = FormBorderStyle.None;
-		StartPosition = FormStartPosition.Manual;
-		ShowInTaskbar = false;
-		TopMost = true;
-
-		BackColor = Color.Black;
-		Opacity = _settings.Opacity;
-		Size = new Size(
-			_rect.Right - _rect.Left - BORDER_MARGIN,
-			_rect.Bottom - _rect.Top - BORDER_MARGIN
-		);
-		Location = new Point(_rect.Left + BORDER_MARGIN / 2, _rect.Top + BORDER_MARGIN / 2);
 	}
 
 	protected override void OnPaint(PaintEventArgs e)

WindowKeys/NativeHelper.cs
@@ -59,12 +59,11 @@ public partial class NativeHelper(ILogger<INativeHelper> logger) : INativeHelper
 		var sw = Stopwatch.StartNew();
 		var visibleWindows = new List<Window>();
 		var nexthWnd = GetTopWindow(nint.Zero);
-		var hWnd = nint.Zero;
+		var prevhWnd = nint.Zero;
 
 		while (nexthWnd != nint.Zero)
 		{
-			var prevhWnd = hWnd;
-			hWnd = nexthWnd;
+			var hWnd = nexthWnd;
 			nexthWnd = GetWindow(hWnd, GW_HWNDNEXT);
 
 			var title = GetWindowTitle(hWnd);
@@ -75,6 +74,7 @@ public partial class NativeHelper(ILogger<INativeHelper> logger) : INativeHelper
 			if (visibleWindows.Any(x => Geometry.IsRectInside(x.Rect, rect))) continue;
 
 			visibleWindows.Add(new Window { Rect = rect, Handle = hWnd, InsertAfter = prevhWnd });
+			prevhWnd = hWnd;
 		}
 
 		if (sw.ElapsedMilliseconds > 100)

WindowKeys/Window.cs
@@ -14,7 +14,7 @@ public class Window
 
 	public void DismissOverlay()
 	{
-		OverlayForm?.Close();
+		OverlayForm?.Hide();
 		Dismissed = true;
 	}
 }

WindowKeys/WindowHandler.cs
@@ -17,6 +17,7 @@ public partial class WindowHandler(
 	) : IWindowHandler
 {
 	private readonly OverlaySettings _overlaySettings = overlaySettings.Value;
+	private readonly List<OverlayForm> _overlayForms = [];
 	private List<Window> Windows { get; set; } = [];
 
 	public bool TestActivationString(string input)
@@ -51,6 +52,11 @@ public partial class WindowHandler(
 
 		var combinations = combinationGenerator.GetCombinations(Windows.Count);
 		var cIndex = 0;
+		var formIndex = 0;
+
+		// Add new forms if required
+		while (_overlayForms.Count < Windows.Count)
+			_overlayForms.Add(new OverlayForm(_overlaySettings, geometry));
 
 		foreach (var win in Windows
 					 .OrderBy(x => x.Rect.Left)
@@ -63,9 +69,11 @@ public partial class WindowHandler(
 
 			var occludingRects = GetOccludingRects(win, Windows);
 
-			win.OverlayForm = new OverlayForm(win.Rect, win.ActivationString, _overlaySettings, geometry, occludingRects);
-			win.OverlayForm.Show();
-			nativeHelper.ZOrderInsertWindowAfter(win.OverlayForm.Handle, win.InsertAfter);
+			var form = _overlayForms[formIndex++];
+			form.Configure(win.Rect, win.ActivationString, occludingRects);
+			win.OverlayForm = form;
+			form.Show();
+			nativeHelper.ZOrderInsertWindowAfter(form.Handle, win.InsertAfter);
 		}
 
 		if (sw.Elapsed.TotalMilliseconds > 200)

commit 9caeb1ba711307be3dfb9135fd61490b1bfaf36a

Fix Overlay Window Z-order
3e45ba6e91353169a8ab31972624f1f3827f45f7 introduced an issue where the
overlay windows did'nt always appear in the correct order. Invalidate
ensures repaint.

WindowKeys/Forms/OverlayForm.cs
@@ -38,6 +38,7 @@ public class OverlayForm : Form
 			_rect.Bottom - _rect.Top - BORDER_MARGIN
 		);
 		Location = new Point(_rect.Left + BORDER_MARGIN / 2, _rect.Top + BORDER_MARGIN / 2);
+		Invalidate();
 	}
 
 	protected override CreateParams CreateParams