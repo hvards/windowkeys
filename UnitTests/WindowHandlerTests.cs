hvards/windowkeys

commit 3e45ba6e91353169a8ab31972624f1f3827f45f7

Reuse Windows for Faster Activation (#2)
Avoid creating new Windows on every activation. Enabling hidden Windows
is substantially faster.

WindowKeys/Forms/OverlayForm.cs
@@ -8,22 +8,35 @@ public class OverlayForm : Form
 {
 	private readonly OverlaySettings _settings;
 	private readonly IGeometry _geometry;
-	private string _activationString;
-	private readonly RECT _rect;
-	private readonly IReadOnlyList<RECT> _occludingRects;
+	private string _activationString = string.Empty;
+	private RECT _rect;
+	private IReadOnlyList<RECT> _occludingRects = [];
 
 	private const int BORDER_MARGIN = 16;
 
-	public OverlayForm(RECT rect, string activationString, OverlaySettings settings, IGeometry geometry,
-		IReadOnlyList<RECT> occludingRects)
+	public OverlayForm(OverlaySettings settings, IGeometry geometry)
 	{
 		_settings = settings;
 		_geometry = geometry;
-		_activationString = activationString;
+
+		FormBorderStyle = FormBorderStyle.None;
+		StartPosition = FormStartPosition.Manual;
+		ShowInTaskbar = false;
+		BackColor = Color.Black;
+		Opacity = _settings.Opacity;
+	}
+
+	public void Configure(RECT rect, string activationString, IReadOnlyList<RECT> occludingRects)
+	{
 		_rect = rect;
+		_activationString = activationString;
 		_occludingRects = occludingRects;
 
-		SetFormAppearanceAndPosition();
+		Size = new Size(
+			_rect.Right - _rect.Left - BORDER_MARGIN,
+			_rect.Bottom - _rect.Top - BORDER_MARGIN
+		);
+		Location = new Point(_rect.Left + BORDER_MARGIN / 2, _rect.Top + BORDER_MARGIN / 2);
 	}
 
 	protected override CreateParams CreateParams
@@ -34,22 +47,6 @@ public class OverlayForm : Form
 			cp.ExStyle |= 0x08000000; // WS_EX_NOACTIVATE 
 			return cp;
 		}
-	}
-
-	private void SetFormAppearanceAndPosition()
-	{
-		FormBorderStyle = FormBorderStyle.None;
-		StartPosition = FormStartPosition.Manual;
-		ShowInTaskbar = false;
-		TopMost = true;
-
-		BackColor = Color.Black;
-		Opacity = _settings.Opacity;
-		Size = new Size(
-			_rect.Right - _rect.Left - BORDER_MARGIN,
-			_rect.Bottom - _rect.Top - BORDER_MARGIN
-		);
-		Location = new Point(_rect.Left + BORDER_MARGIN / 2, _rect.Top + BORDER_MARGIN / 2);
 	}
 
 	protected override void OnPaint(PaintEventArgs e)

WindowKeys/NativeHelper.cs
@@ -59,12 +59,11 @@ public partial class NativeHelper(ILogger<INativeHelper> logger) : INativeHelper
 		var sw = Stopwatch.StartNew();
 		var visibleWindows = new List<Window>();
 		var nexthWnd = GetTopWindow(nint.Zero);
-		var hWnd = nint.Zero;
+		var prevhWnd = nint.Zero;
 
 		while (nexthWnd != nint.Zero)
 		{
-			var prevhWnd = hWnd;
-			hWnd = nexthWnd;
+			var hWnd = nexthWnd;
 			nexthWnd = GetWindow(hWnd, GW_HWNDNEXT);
 
 			var title = GetWindowTitle(hWnd);
@@ -75,6 +74,7 @@ public partial class NativeHelper(ILogger<INativeHelper> logger) : INativeHelper
 			if (visibleWindows.Any(x => Geometry.IsRectInside(x.Rect, rect))) continue;
 
 			visibleWindows.Add(new Window { Rect = rect, Handle = hWnd, InsertAfter = prevhWnd });
+			prevhWnd = hWnd;
 		}
 
 		if (sw.ElapsedMilliseconds > 100)

WindowKeys/Window.cs
@@ -14,7 +14,7 @@ public class Window
 
 	public void DismissOverlay()
 	{
-		OverlayForm?.Close();
+		OverlayForm?.Hide();
 		Dismissed = true;
 	}
 }

WindowKeys/WindowHandler.cs
@@ -17,6 +17,7 @@ public partial class WindowHandler(
 	) : IWindowHandler
 {
 	private readonly OverlaySettings _overlaySettings = overlaySettings.Value;
+	private readonly List<OverlayForm> _overlayForms = [];
 	private List<Window> Windows { get; set; } = [];
 
 	public bool TestActivationString(string input)
@@ -51,6 +52,11 @@ public partial class WindowHandler(
 
 		var combinations = combinationGenerator.GetCombinations(Windows.Count);
 		var cIndex = 0;
+		var formIndex = 0;
+
+		// Add new forms if required
+		while (_overlayForms.Count < Windows.Count)
+			_overlayForms.Add(new OverlayForm(_overlaySettings, geometry));
 
 		foreach (var win in Windows
 					 .OrderBy(x => x.Rect.Left)
@@ -63,9 +69,11 @@ public partial class WindowHandler(
 
 			var occludingRects = GetOccludingRects(win, Windows);
 
-			win.OverlayForm = new OverlayForm(win.Rect, win.ActivationString, _overlaySettings, geometry, occludingRects);
-			win.OverlayForm.Show();
-			nativeHelper.ZOrderInsertWindowAfter(win.OverlayForm.Handle, win.InsertAfter);
+			var form = _overlayForms[formIndex++];
+			form.Configure(win.Rect, win.ActivationString, occludingRects);
+			win.OverlayForm = form;
+			form.Show();
+			nativeHelper.ZOrderInsertWindowAfter(form.Handle, win.InsertAfter);
 		}
 
 		if (sw.Elapsed.TotalMilliseconds > 200)

commit 20dab02e7d5c8ab1546bea16b31eb9d9076d71fa

Remove Transparent Overlay (#5)
* Fix Overlay Window Z-order

3e45ba6e91353169a8ab31972624f1f3827f45f7 introduced an issue where the
overlay windows did'nt always appear in the correct order. Invalidate
ensures repaint.

* Remove Transparent Overlay

WindowKeys/Forms/OverlayForm.cs
@@ -1,3 +1,5 @@
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
 using WindowKeys.Interfaces;
 using WindowKeys.Settings;
 using WindowKeys.Native;
@@ -11,20 +13,20 @@ public class OverlayForm : Form
 	private string _activationString = string.Empty;
 	private RECT _rect;
 	private IReadOnlyList<RECT> _occludingRects = [];
-
-	private const int BORDER_MARGIN = 16;
+	private readonly Font _font;
+	private Size _textSize;
 
 	public OverlayForm(OverlaySettings settings, IGeometry geometry)
 	{
 		_settings = settings;
 		_geometry = geometry;
+		_font = new Font(_settings.FontFamily, _settings.FontSize);
 
 		FormBorderStyle = FormBorderStyle.None;
 		StartPosition = FormStartPosition.Manual;
 		ShowInTaskbar = false;
 		TopMost = true;
-		BackColor = Color.Black;
-		Opacity = _settings.Opacity;
+		BackColor = ColorTranslator.FromHtml(_settings.BackgroundColor);
 	}
 
 	public void Configure(RECT rect, string activationString, IReadOnlyList<RECT> occludingRects)
@@ -32,12 +34,14 @@ public class OverlayForm : Form
 		_rect = rect;
 		_activationString = activationString;
 		_occludingRects = occludingRects;
+		LayoutOverlay();
+	}
 
-		Size = new Size(
-			_rect.Right - _rect.Left - BORDER_MARGIN,
-			_rect.Bottom - _rect.Top - BORDER_MARGIN
-		);
-		Location = new Point(_rect.Left + BORDER_MARGIN / 2, _rect.Top + BORDER_MARGIN / 2);
+	public void UpdateActivationString(string activationString)
+	{
+		_activationString = activationString;
+		_textSize = TextRenderer.MeasureText(_activationString, _font, Size.Empty, TextFormatFlags.NoPadding);
+		Invalidate();
 	}
 
 	protected override CreateParams CreateParams
@@ -45,7 +49,7 @@ public class OverlayForm : Form
 		get
 		{
 			var cp = base.CreateParams;
-			cp.ExStyle |= 0x08000000; // WS_EX_NOACTIVATE 
+			cp.ExStyle |= 0x08000000; // WS_EX_NOACTIVATE
 			return cp;
 		}
 	}
@@ -53,42 +57,54 @@ public class OverlayForm : Form
 	protected override void OnPaint(PaintEventArgs e)
 	{
 		base.OnPaint(e);
+		e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+		e.Graphics.TextRenderingHint = TextRenderingHint.AntiAlias;
 		DrawBorder(e.Graphics);
 		DrawActivationString(e.Graphics);
 	}
 
-	private void DrawBorder(Graphics graphics)
+	private void LayoutOverlay()
 	{
-		var rectangle = new Rectangle(0, 0, Size.Width, Size.Height);
-		using var pen = new Pen(ColorTranslator.FromHtml(_settings.BorderColor), _settings.BorderWidth);
-		graphics.DrawRectangle(pen, rectangle);
+		_textSize = TextRenderer.MeasureText(_activationString, _font, Size.Empty, TextFormatFlags.NoPadding);
+		var position = _geometry.GetActivationStringPosition(_rect, _occludingRects, _textSize);
+		if (position == null) return;
+
+		var w = _textSize.Width + 24 + _settings.BorderWidth + 1;
+		var h = _textSize.Height + _settings.BorderWidth + 1;
+		Size = new Size(w, h);
+		Location = new Point(position.Value.X - w / 2, position.Value.Y - h / 2);
+
+		var inset = _settings.BorderWidth / 2f;
+		var radius = new SizeF(_settings.CornerRadius, _settings.CornerRadius);
+		var rect = new RectangleF(inset, inset,
+				ClientSize.Width - _settings.BorderWidth - 1,
+				ClientSize.Height - _settings.BorderWidth - 1);
+
+		using var path = new GraphicsPath();
+		path.AddRoundedRectangle(rect, radius);
+		Region = new Region(path);
 	}
 
-	public void UpdateActivationString(string activationString)
+	private void DrawBorder(Graphics graphics)
 	{
-		_activationString = activationString;
-		Invalidate();
+		using var pen = new Pen(ColorTranslator.FromHtml(_settings.BorderColor), _settings.BorderWidth);
+
+		float bw = pen.Width;
+		float inset = bw / 2f;
+
+		var rect = new RectangleF(
+			inset,
+			inset,
+			ClientSize.Width - bw - 1f,
+			ClientSize.Height - bw - 1f);
+
+		var radius = new SizeF(_settings.CornerRadius, _settings.CornerRadius);
+		graphics.DrawRoundedRectangle(pen, rect, radius);
 	}
 
 	private void DrawActivationString(Graphics graphics)
 	{
-		if (_settings.FontSize <= 0) return;
-		using var font = new Font(_settings.FontFamily, _settings.FontSize);
-		var size = graphics.MeasureString(_activationString, font).ToSize();
-		var position = _geometry.GetActivationStringPosition(_rect, _occludingRects, size);
-		if (position == null) return;
-
-		var location = new Point(
-			position.Value.X - Location.X - size.Width / 2,
-			position.Value.Y - Location.Y - size.Height / 2);
-
-		var rectangle = new Rectangle(location, size);
-		var textPosition = new PointF(
-			rectangle.Left + (1f * rectangle.Width - size.Width) / 2,
-			rectangle.Top + (1f * rectangle.Height - size.Height) / 2
-		);
-
-		using var brush = new SolidBrush(Color.White);
-		graphics.DrawString(_activationString, font, brush, textPosition);
+		var position = new Point((Width - _textSize.Width) / 2, (Height - _textSize.Height) / 2);
+		TextRenderer.DrawText(graphics, _activationString, _font, position, Color.White, TextFormatFlags.NoPadding);
 	}
 }

WindowKeys/Interfaces/INativeHelper.cs
@@ -9,7 +9,6 @@ public interface INativeHelper
 	nint CallNextHook(nint hhk, int nCode, nint wParam, ref KeyboardInput lParam);
 	List<Window> GetWindowsInZOrder();
 	void FocusHandle(nint handle);
-	bool IsWindowAtPosition(nint windowHandle, int x, int y);
 	void ZOrderInsertWindowAfter(nint windowHandle, nint hWndInsertAfter);
 	void ClickKey(ushort vk, nint? action);
 }

WindowKeys/Native/Constants.cs
@@ -3,14 +3,6 @@
 public static class Constants
 {
 	internal const int WH_KEYBOARD_LL = 13;
-	internal const uint WM_CLOSE = 0x0010;
-	internal const int SW_MINIMIZE = 6;
-	internal const int GCL_HICONSM = -34;
-	internal const int GCL_HICON = -14;
-	internal const int WM_GETICON = 0x7f;
-	internal const int ICON_SMALL = 0;
-	internal const int ICON_BIG = 1;
-	internal const int ICON_SMALL2 = 2;
 	public const nint WM_KEYDOWN = 0x0100;
 	public const nint WM_KEYUP = 0x0101;
 	internal const nint WM_SYSKEYDOWN = 0x0104;

WindowKeys/NativeHelper.cs
@@ -48,12 +48,6 @@ public partial class NativeHelper(ILogger<INativeHelper> logger) : INativeHelper
 		_ = SetWindowPos(windowHandle, hWndInsertAfter, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
 	}
 
-	public bool IsWindowAtPosition(nint windowHandle, int x, int y)
-	{
-		var point = new Point(x, y);
-		return WindowFromPoint(point) == windowHandle;
-	}
-
 	public List<Window> GetWindowsInZOrder()
 	{
 		var sw = Stopwatch.StartNew();
@@ -88,14 +82,8 @@ public partial class NativeHelper(ILogger<INativeHelper> logger) : INativeHelper
 	private static void SendKeyboardInput(Input[] kbInputs) =>
 		_ = SendInput((uint)kbInputs.Length, kbInputs, Marshal.SizeOf<Input>());
 
-	private static Input[] GetKeyboardInputArr(ushort vk, ushort modifier = 0, nint? action = null) => action == null
-		? modifier == 0
-			? [GetKeyboardInput(vk, true), GetKeyboardInput(vk, false)]
-			:
-			[
-				GetKeyboardInput(modifier, true), GetKeyboardInput(vk, true), GetKeyboardInput(vk, false),
-				GetKeyboardInput(modifier, false)
-			]
+	private static Input[] GetKeyboardInputArr(ushort vk, nint? action = null) => action == null
+		? [GetKeyboardInput(vk, true), GetKeyboardInput(vk, false)]
 		: [GetKeyboardInput(vk, (int)action == (int)WM_KEYDOWN)];
 
 	private static Input GetKeyboardInput(ushort vk, bool down) => new()
@@ -181,9 +169,6 @@ public partial class NativeHelper(ILogger<INativeHelper> logger) : INativeHelper
 	[return: MarshalAs(UnmanagedType.Bool)]
 	private static partial bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy,
 		uint uFlags);
-
-	[DllImport("user32.dll")]
-	private static extern IntPtr WindowFromPoint(Point p);
 
 	[LibraryImport("kernel32.dll", EntryPoint = "GetModuleHandleW", StringMarshalling = StringMarshalling.Utf16)]
 	private static partial nint GetModuleHandle(string lpModuleName);

WindowKeys/Settings/OverlaySettings.cs
@@ -4,7 +4,8 @@ public class OverlaySettings
 {
 	public string FontFamily { get; set; } = string.Empty;
 	public int FontSize { get; set; }
+	public string BackgroundColor { get; set; } = string.Empty;
 	public string BorderColor { get; set; } = string.Empty;
-	public float BorderWidth { get; set; }
-	public double Opacity { get; set; }
+	public int BorderWidth { get; set; }
+	public int CornerRadius { get; set; }
 }

UnitTests/WindowHandlerTests.cs
@@ -20,7 +20,13 @@ public class WindowHandlerTests
 		_nativeHelper = new Mock<INativeHelper>();
 		_combinationGenerator = new Mock<ICombinationGenerator>();
 		_geometry = new Mock<IGeometry>();
-		var settings = new OverlaySettings();
+		var settings = new OverlaySettings()
+		{
+			FontFamily = "Arial",
+			FontSize = 12,
+			BackgroundColor = "#111111",
+			BorderColor = "#222222"
+		};
 
 		_subject = new WindowHandler(_nativeHelper.Object, _combinationGenerator.Object, _geometry.Object,
 			new NullLogger<WindowHandler>(), Options.Create(settings));